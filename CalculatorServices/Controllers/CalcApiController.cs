69e68b13615586404374c7404dc42e94c702595d
elliottchen <s.chen@kodnet.co.jp>
2019-01-07 11:10:50 +0900

    create asp.net mvc project

---
+++ CalculatorServices/Controllers/CalcApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculatorServices.Controllers
+{
+    public class CalcApiController : Controller
+    {
+        // GET: CalcApi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
