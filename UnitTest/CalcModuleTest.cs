a38489e27d60998f1ad2dd232fb122de41fd1144
elliottchen <s.chen@kodnet.co.jp>
2019-01-07 17:45:37 +0900

    implement UnitTest

---
+++ UnitTest/CalcModuleTest.cs
@@ -0,0 +1,164 @@
+﻿using CalculatorServices.Enums;
+using CalculatorServices.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CalcModuleTest
+    {
+        #region Add
+
+        [TestMethod]
+        public void CanAddPositiveNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Add(100.01m, 200.033m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(300.043m, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void CanAddNegativeNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Add(-3.99m, -199.01m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(-203, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void AddNumberOverflow()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Add(decimal.MaxValue, decimal.MaxValue);
+
+            Assert.AreEqual(ResultType.OverFlow, result.ResultCode);
+        }
+
+        #endregion // Add
+
+        #region Subtract
+
+        [TestMethod]
+        public void CanSubtractPositiveNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Subtract(100.033m, 200.033m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(-100m, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void CanSubtractNegativeNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Subtract(-100.033m, -200.033m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(100m, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void SubstractNumberOverflow()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Subtract(decimal.MaxValue, decimal.MinValue);
+
+            Assert.AreEqual(ResultType.OverFlow, result.ResultCode);
+        }
+
+        #endregion // Subtract
+
+        #region Multiply
+
+        [TestMethod]
+        public void CanMultiplyPositiveNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Multiply(100.033m, 200.033m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(20009.901089m, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void CanMultiplyNegativeNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Multiply(-100.033m, -200.033m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(20009.901089m, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void MultiplyNumberOverflow()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Multiply(decimal.MaxValue, decimal.MaxValue);
+
+            Assert.AreEqual(ResultType.OverFlow, result.ResultCode);
+        }
+
+        #endregion // Multiply
+
+        #region Divide
+
+        [TestMethod]
+        public void CanDividePositiveNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Divide(200.5m, 2.5m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(80.2m, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void CanDivideNegativeNumber()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Divide(200.5m, -2.5m);
+
+            Assert.AreEqual(ResultType.Success, result.ResultCode);
+            Assert.AreEqual(-80.2m, result.OperationResult);
+        }
+
+        [TestMethod]
+        public void DivideNumberOverflow()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Divide(decimal.MaxValue, 0.1m);
+
+            Assert.AreEqual(ResultType.OverFlow, result.ResultCode);
+        }
+
+        [TestMethod]
+        public void DivisorIsZero()
+        {
+            var calcModule = new CalcModule();
+
+            var result = calcModule.Divide(decimal.MaxValue, 0);
+
+            Assert.AreEqual(ResultType.ZeroDivisor, result.ResultCode);
+        }
+
+        #endregion // Divide
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9e103aa-2ee1-44a0-985c-8d625d4c6763")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
